ea397888cc1a0759cdc3d3f4f01ef8ea849b826e
dingge2016 <dingyue@usc.edu>
2020-02-18 21:46:25 -0800

    Merge Car

--- Assets/Scripts/CarControl.cs
+++ Assets/Scripts/CarControl.cs
@@ -8,10 +8,14 @@ public class CarControl : MonoBehaviour
 
 
     private Vector3 screenPoint;
-    private Vector3 offset;
-    public GameObject BackCarPart;
+    private Vector3 offset; 
     private Vector3 newPosition;
 
+    private float leftOffset = -0.5f;
+    private float rightOffset = 0.5f;
+    private float centerOffset = 0;
+    //private float speed = 20.0f;
+    
 
 
     void OnMouseDown()
@@ -26,8 +30,8 @@ public class CarControl : MonoBehaviour
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 
 
-        int diffx = (int)curPosition.x  - (int)transform.position.x;
-        int diffy = (int)curPosition.y - (int)transform.position.y;
+        float diffx = curPosition.x  - transform.position.x;
+        float diffy = curPosition.y - transform.position.y;
 
         int dx = 0;
         int dy = 0;
@@ -41,21 +45,27 @@ public class CarControl : MonoBehaviour
             dy = 1;
         else if (diffy <= -1)
             dy = -1;
-
-        int nx = (int)transform.position.x + dx;
+        // the position of left side of the car
+        float leftx = transform.position.x + dx + leftOffset;
+        // the position of right side of the car
+        float rightx = transform.position.x + dx + rightOffset;
+        float nx = transform.position.x + dx + centerOffset;
         int ny = (int)transform.position.y + dy;
-
+        Debug.Log(leftx.ToString() + " " + nx.ToString() + " " + rightx.ToString());
         // detect whether there is a wall in the front part of the car or back part of the car.
-        if (isWall(nx, ny) || isWall(nx-1, ny)) return;
+        if (isWall((int)leftx, ny) || isWall((int)rightx, ny)) return;
 
         // detect Exit
-        if (isExit(nx, ny)){
+        if (isExit((int)leftx, ny)){
           gameObject.SetActive(false);
         }
 
-        // Move player to next position.
         transform.position = new Vector3(nx, ny);
-        BackCarPart.transform.position = new Vector3(nx-1, ny);
+        /*// Move our position a step closer to the target.
+        float step = speed * Time.deltaTime; // calculate distance to move
+        // Move player to next position.
+        transform.position = transform.position = Vector3.MoveTowards(transform.position, new Vector3(nx, ny), step);
+        Debug.Log(transform.position.x.ToString() +" " + transform.position.y.ToString() + " " + nx.ToString() +" " + ny.ToString());*/
     }
 
 
