3341f633410e74634f9e51ee8f766da1ee4708d0
XiaotongJiang <32645163+CallMeMajorTom@users.noreply.github.com>
2020-02-18 19:01:06 -0800

    Car Exit

--- Assets/Scripts/MapCreater.cs
+++ Assets/Scripts/MapCreater.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class MapCreater : MonoBehaviour
 {
     public string[] map;
-
+  //  {
+  //  "..UUUUUU..",
+  //  ".L......R.",
+  //  ".L......R.",
+  //  "UL......RU",
+  //  "I........O",
+  //  "DL......RD",
+  //  ".L......R.",
+  //  ".L......R.",
+  //  "..DDDDDD.."
+  //  };
     public GameObject Upwall;
     public GameObject Downwall;
     public GameObject Leftwall;
@@ -16,13 +26,14 @@ public class MapCreater : MonoBehaviour
     public GameObject Ground;
 
     private HashSet<int> wall_pos_set ;
+    private Vector3 exitPos;
 
     // use to convert 2D position to 1D position.
     public const int SIZE = 1000;
 
     // Left top position
     public int left_top_x = -5;
-    public int left_top_y = -4;
+    public int left_top_y = 4;
 
     private void Awake()
     {
@@ -39,7 +50,6 @@ public class MapCreater : MonoBehaviour
             for (int i = 0; i < row.Length; ++i)
             {
                 Vector3 cell_pos = new Vector3(row_pos, col_pos);
-
                 if (row[i] == 'U') //Upper Wall
                 {
                     Instantiate(Upwall, cell_pos, Quaternion.identity);
@@ -67,6 +77,7 @@ public class MapCreater : MonoBehaviour
                 else if (row[i] == 'O') //Exit
                 {
                     GameObject exit = Instantiate(Exit, cell_pos, Quaternion.identity);
+                    exitPos = new Vector3(row_pos, col_pos);
                 }
                 // Ground
                 Instantiate(Ground, cell_pos, Quaternion.identity);
@@ -78,6 +89,10 @@ public class MapCreater : MonoBehaviour
 
     public HashSet<int> getWallPosSet() {
         return wall_pos_set;
+    }
+
+    public Vector3 getExitPos() {
+        return exitPos;
     }
 
     public int TwoDToOneD(int x, int y) {
