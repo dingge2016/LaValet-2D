47a682c7a42f308b4dccedc58fd1197b22153c53
Amber Chang <amberc@usc.edu>
2020-02-19 05:05:35 -0800

    Added timers

---
+++ Assets/Scripts/CarTimer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startTime = 0f;
+    [SerializeField] Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Random.Range(5.0f, 15.0f);
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;     // Time.deltaTime to make time be updated by second not by frame
+
+        int seconds = (int)(currentTime % 60);
+        int minutes = (int)(currentTime / 60);
+
+        string timerString = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        if (currentTime <= 6)
+        {
+            countdownText.color = Color.red;
+        }
+        else
+        {
+            countdownText.color = Color.white;
+        }
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+
+        Vector3 carTimerPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        countdownText.transform.position = carTimerPos;
+        countdownText.text = timerString;
+    }
+}
---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    [SerializeField] float startTime;
+    [SerializeField] Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;     // Time.deltaTime to make time be updated by second not by frame
+
+        int seconds = (int)(currentTime % 60);
+        int minutes = (int)(currentTime / 60);
+
+        string timerString = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        countdownText.text = timerString;
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
